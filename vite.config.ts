384e80b008090a9b52aed19b8a4b3faf24caa3ed
imv <imvenx@proton.me>
2023-04-26 20:39:38 +0200

    fix error

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'development' ? '' : '/arcanepad/',
+  publicDir: process.env.NODE_ENV === 'development' ? '' : '/arcanepad/',
   build: {
     outDir: 'docs'
   }
