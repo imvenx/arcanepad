d7c3070f380f80fe7d1ad6b77140ee691c9bc60f
imv <imvenx@proton.me>
2023-04-26 20:27:18 +0200

    add downloads v0.0.1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main style="text-align: center;">
+    <h1>Arcanepad 0.0.1</h1>
+
+
+    <a href="/downloads/desktop/opengaming_0.0.1_amd64.snap">
+      <!-- <a href="../../public//downloads/desktop/opengaming_0.0.1_amd64.snap"> -->
+      <h3>
+        Destkop download
+      </h3>
+    </a>
+    <br />
+    <br />
+    <a href="/downloads/android/app-release_0.0.1.apk">
+      <h3>
+        Android download
+      </h3>
+    </a>
   </main>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+h3 {
+  display: inline;
+}
+</style>
