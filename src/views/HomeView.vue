0bfc08be00839a9ea73bcfa0eb79d1d432225c73
imv <imvenx@proton.me>
2023-04-27 21:30:27 +0200

    b

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,14 @@
     <h1>Arcanepad 0.0.1</h1>
 
 
-    <a href="downloads/desktop/arcanepad_0.0.1.AppImage">
+    <a href="../downloads/desktop/arcanepad_0.0.1.AppImage">
       <h3>
         Destkop download
       </h3>
     </a>
     <br />
     <br />
-    <a href="downloads/android/arcanepad_0.0.1.apk">
+    <a href="../downloads/android/arcanepad_0.0.1.apk">
       <h3>
         Android download
       </h3>
